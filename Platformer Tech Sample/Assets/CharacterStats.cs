352e129bd95908652b69d6d67c979bfc589f9388
Hamnety <filichkin_02@mail.ru>
2024-12-01 04:01:03 +0300

    MainAdd

---
+++ Platformer Tech Sample/Assets/CharacterStats.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using TMPro;
+using Quantum.Core;
+using Quantum;
+
+public class CharacterStats : MonoBehaviour
+{
+    //public TMP_Text HPCount;
+    public EntityRef Entity;
+    void Start()
+    {
+        //HPCount.text = FrameBase.Get<Health>(Entity);
+        //HPCount.text = UI;
+    }
+
+}
